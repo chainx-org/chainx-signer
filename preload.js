1fef56ac061f9a97987acdc154bb7b2abbaaafbc
yznt <nsso@foxmail.com>
2019-11-28 16:21:19 +0800

    socketResponse

--- preload.js
+++ preload.js
@@ -3,4 +3,13 @@ const sockets = require('./server')
 
 sockets.setMainWindow(electron.remote.getCurrentWindow())
 
+electron.ipcRenderer.on('socketResponse', (event, data) =>
+  window.wallet.socketResponse(data)
+)
+
 window.sockets = sockets
+window.wallet = {
+  socketResponse: () => {
+    console.log('哈哈哈哈哈哈')
+  }
+}
