e1bb60180ec8ae5425713563f9ac01ee7a9e3a19
Yongfeng LI <wliyongfeng@gmail.com>
2019-12-25 16:03:35 +0800

    Refactor server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const { findPort } = require('./utils')
+const requestHandler = require('./httpHandler')
 const http = require('http')
 const WebSocket = require('ws')
 
@@ -37,92 +38,83 @@ class LowLevelSocketService {
     socket.send('42/chainx,' + JSON.stringify([path, data ? data : false]))
   }
 
+  socketHandler(socket) {
+    let origin = null
+
+    socket.send('40')
+    socket.send('40/chainx')
+    socket.send(`42/chainx,["connected"]`)
+
+    const id = Math.round(Math.random() * 999999999).toString()
+
+    // Different clients send different message types for disconnect (ws vs socket.io)
+    socket.on('close', () => delete this.openConnections[origin + id])
+    socket.on('disconnect', () => delete this.openConnections[origin + id])
+
+    socket.on('message', msg => {
+      if (msg.indexOf('42/chainx') === -1) return false
+      const [type, request] = JSON.parse(msg.replace('42/chainx,', ''))
+
+      const killRequest = () =>
+        this.emitSocket(socket, 'api', { id: request.data.id, result: null })
+
+      if (!request.plugin || request.plugin.length > 100) {
+        return killRequest()
+      }
+      request.plugin = request.plugin.replace(/\s/g, '').trim()
+
+      if (request.plugin.toLowerCase() === 'chainx') {
+        return killRequest()
+      }
+
+      let requestOrigin
+      if (request.data.hasOwnProperty('payload')) {
+        request.data.payload.origin = request.data.payload.origin
+          .replace(/\s/g, '')
+          .trim()
+        if (request.data.payload.origin.toLowerCase() === 'chainx') {
+          return killRequest()
+        }
+        requestOrigin = request.data.payload.origin
+      } else {
+        requestOrigin = request.data.origin.replace(/\s/g, '').trim()
+      }
+
+      if (!origin) {
+        origin = requestOrigin
+      } else if (origin && requestOrigin !== origin) {
+        return killRequest()
+      }
+
+      if (!this.openConnections.hasOwnProperty(origin + id)) {
+        this.openConnections[origin + id] = socket
+      }
+
+      switch (type) {
+        case 'pair':
+          return sendToEmbed({ type: 'pair', request, id })
+        case 'rekeyed':
+          return this.rekeyPromise.resolve(request)
+        case 'api':
+          return sendToEmbed({ type: 'api', request, id })
+        default:
+          console.error(`Unknown type ${type}`)
+      }
+    })
+  }
+
   async initialize() {
-    const socketHandler = socket => {
-      let origin = null
-
-      socket.send('40')
-      socket.send('40/chainx')
-      socket.send(`42/chainx,["connected"]`)
-
-      const id = Math.round(Math.random() * 999999999).toString()
-
-      // Different clients send different message types for disconnect (ws vs socket.io)
-      socket.on('close', () => delete this.openConnections[origin + id])
-      socket.on('disconnect', () => delete this.openConnections[origin + id])
-
-      socket.on('message', msg => {
-        if (msg.indexOf('42/chainx') === -1) return false
-        const [type, request] = JSON.parse(msg.replace('42/chainx,', ''))
-
-        const killRequest = () =>
-          this.emitSocket(socket, 'api', { id: request.data.id, result: null })
-
-        if (!request.plugin || request.plugin.length > 100) {
-          return killRequest()
-        }
-        request.plugin = request.plugin.replace(/\s/g, '').trim()
-
-        if (request.plugin.toLowerCase() === 'chainx') {
-          return killRequest()
-        }
-
-        let requestOrigin
-        if (request.data.hasOwnProperty('payload')) {
-          request.data.payload.origin = request.data.payload.origin
-            .replace(/\s/g, '')
-            .trim()
-          if (request.data.payload.origin.toLowerCase() === 'chainx') {
-            return killRequest()
-          }
-          requestOrigin = request.data.payload.origin
-        } else {
-          requestOrigin = request.data.origin.replace(/\s/g, '').trim()
-        }
-
-        if (!origin) {
-          origin = requestOrigin
-        } else if (origin && requestOrigin !== origin) {
-          return killRequest()
-        }
-
-        if (!this.openConnections.hasOwnProperty(origin + id)) {
-          this.openConnections[origin + id] = socket
-        }
-
-        switch (type) {
-          case 'pair':
-            return sendToEmbed({ type: 'pair', request, id })
-          case 'rekeyed':
-            return this.rekeyPromise.resolve(request)
-          case 'api':
-            return sendToEmbed({ type: 'api', request, id })
-          default:
-            console.error(`Unknown type ${type}`)
-        }
-      })
-    }
-
     if (this.websocket) {
       return this.websocket
     }
 
     this.port = await findPort()
 
-    const requestHandler = (_, res) => {
-      res.setHeader('Access-Control-Allow-Origin', '*')
-      res.setHeader('Access-Control-Request-Method', '*')
-      res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
-      res.setHeader('Access-Control-Allow-Headers', '*')
-      res.setHeader('Content-Type', 'application/json')
-      res.end('chainx')
-    }
-
     const server = http.createServer(requestHandler)
     this.websocket = new WebSocket.Server({ server })
     server.listen(this.port)
 
-    this.websocket.on('connection', socketHandler)
+    this.websocket.on('connection', this.socketHandler.bind(this))
     return this.websocket
   }
 
