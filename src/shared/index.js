6b88d40b730c87fd6bc3e4eb881c1e6dcb61724b
XatMassacrE <xxat.massacre@gmail.com>
2019-12-31 11:43:59 +0800

    Delete some useless files

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,6 +1,3 @@
-export { default as useRedux } from './useRedux'
-export { default as createStore } from './createStore'
-export { default as shallowEqual } from './shallowEqual'
 export { default as useOutsideClick } from './useClickOutside'
 export { default as fetchFromWs } from './fetch'
 export { default as getDelay } from './updateNodeStatus'
