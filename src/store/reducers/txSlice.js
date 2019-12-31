da2e5f8088c2078de580b996a410758f2949cac7
Yongfeng LI <wliyongfeng@gmail.com>
2019-12-31 17:56:22 +0800

    Remove the mock toSign

--- src/store/reducers/txSlice.js
+++ src/store/reducers/txSlice.js
@@ -1,18 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const mockToSign = {
-  origin: '',
-  id: 'test',
-  dataId: '',
-  address: '5Fe4i2nqi1yPaMowBmjYKb7eRmRYgSE4QAgpxJq1kaGdvQL8',
-  data:
-    '0xdc010803ff9e25fd5dc4cc26f921ad805ef234217dbde234910b526f0434f576d8ad2d77830c50435800e1f5050000000018e598bbe598bb'
-}
-
 const initialState = {
   version: 0,
-  // toSign: null,
-  toSign: mockToSign
+  toSign: null
 }
 
 const txSlice = createSlice({
