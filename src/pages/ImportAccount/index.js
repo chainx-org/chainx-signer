b43e090b71a894db07bb89df66b08037cefd5298
Yongfeng LI <wliyongfeng@gmail.com>
2020-02-10 20:07:19 +0800

    Check mnemonic validity

--- src/pages/ImportAccount/index.js
+++ src/pages/ImportAccount/index.js
@@ -3,6 +3,7 @@ import './importAccount.scss'
 import ErrorMessage from '../../components/ErrorMessage'
 import NameAndPassword from '../../components/NameAndPassword'
 import { TextInput } from '@chainx/ui'
+import { Account } from 'chainx.js'
 
 function ImportAccount(props) {
   const [currentStep, setCurrentStep] = useState(0)
@@ -22,7 +23,10 @@ function ImportAccount(props) {
 
   const checkStep1 = () => {
     if (currentTabIndex === 0) {
-      if (mnemonicList.some(item => item === '')) {
+      if (
+        mnemonicList.some(item => item === '') ||
+        !Account.isMnemonicValid(mnemonicList.join(' '))
+      ) {
         setErrMsg('Mnemonic is not correct')
         return
       }
