7d0d66dabdf0a300cd24b20cd2b0fb75777db7c2
Yongfeng LI <wliyongfeng@gmail.com>
2020-03-04 11:24:02 +0800

    Show method name in header of sign page

--- src/pages/Header/SignHeader.js
+++ src/pages/Header/SignHeader.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { toSignExtrinsicSelector } from '../../store/reducers/txSlice'
+import { useSelector } from 'react-redux'
 
-export default function({ history }) {
+export default function() {
+  const extrinsic = useSelector(toSignExtrinsicSelector)
+  const methodName = extrinsic && extrinsic.methodName
+
   return (
     <div className="center-title">
       <span>
-        {((history.location.query && history.location.query.method) || '')
-          .replace(/([A-Z])/g, ' $1')
-          .toLowerCase() || 'Sign Request'}
+        {(methodName || '').replace(/([A-Z])/g, ' $1').toLowerCase() ||
+          'Sign Request'}
       </span>
     </div>
   )
