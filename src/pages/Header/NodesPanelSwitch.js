b10ce604ed890a9aa1faba5aa3140a894fed2b5f
Yongfeng LI <wliyongfeng@gmail.com>
2020-03-05 22:58:30 +0800

    Extract styled components

--- src/pages/Header/NodesPanelSwitch.js
+++ src/pages/Header/NodesPanelSwitch.js
@@ -11,6 +11,27 @@ import {
   currentChainxNodeSelector
 } from '../../store/reducers/nodeSlice'
 import { useOutsideClick } from '../../shared'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  font-size: 13px;
+  height: 32px;
+  width: 123px;
+  border-radius: 16px;
+  border: 1px solid #dce0e2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 26px;
+  cursor: pointer;
+`
+
+const Dot = styled.span`
+  width: 6px;
+  height: 6px;
+  border-radius: 3px;
+  margin-right: 6px;
+`
 
 export default function() {
   const refNodeList = useRef(null)
@@ -24,18 +45,15 @@ export default function() {
   })
 
   return (
-    <div
+    <Wrapper
       ref={refNodeList}
-      className="current-node"
       onClick={() => {
         dispatch(setShowNodeMenu(!showNodeMenu))
         dispatch(setShowAccountMenu(false))
       }}
     >
-      <span
-        className={'dot ' + getDelayClass(nodesDelay[currentNode.url]) + '-bg'}
-      />
+      <Dot className={getDelayClass(nodesDelay[currentNode.url]) + '-bg'} />
       <span>{currentNode && currentNode.name}</span>
-    </div>
+    </Wrapper>
   )
 }
--- src/pages/Header/SignHeader.js
+++ src/pages/Header/SignHeader.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { toSignExtrinsicSelector } from '../../store/reducers/txSlice'
 import { useSelector } from 'react-redux'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  font-weight: 500;
+  font-size: 20px;
+  color: #3f3f3f;
+  flex: 1;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
 
 export default function() {
   const extrinsic = useSelector(toSignExtrinsicSelector)
   const methodName = extrinsic && extrinsic.methodName
 
   return (
-    <div className="center-title">
-      <span>
-        {(methodName || '').replace(/([A-Z])/g, ' $1').toLowerCase() ||
-          'Sign Request'}
-      </span>
-    </div>
+    <Wrapper>
+      {(methodName || '').replace(/([A-Z])/g, ' $1').toLowerCase() ||
+        'Sign Request'}
+    </Wrapper>
   )
 }
