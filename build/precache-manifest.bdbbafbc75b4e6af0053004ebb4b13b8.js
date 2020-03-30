62bcaed16f19b165a7f3c63ae4c6493931f7885e
Yongfeng LI <wliyongfeng@gmail.com>
2020-03-30 17:33:37 +0800

    Try different relative path

---
+++ build/precache-manifest.bdbbafbc75b4e6af0053004ebb4b13b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c4665204f0d8a6372a58050bd466a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3fd087a72a78a53d432",
+    "url": "/static/css/main.08a032af.chunk.css"
+  },
+  {
+    "revision": "b28bbea5ef4d9a4c6a4f",
+    "url": "/static/js/2.35c70137.chunk.js"
+  },
+  {
+    "revision": "c3fd087a72a78a53d432",
+    "url": "/static/js/main.fc07b869.chunk.js"
+  },
+  {
+    "revision": "ac9aae15256d6bbc5bec",
+    "url": "/static/js/runtime-main.99516cff.js"
+  },
+  {
+    "revision": "7549fcc96c8382912aececfe46cdb77c",
+    "url": "/static/media/iconfont.7549fcc9.eot"
+  },
+  {
+    "revision": "77c882e5f33deb1ac02c2e1e5fae7033",
+    "url": "/static/media/iconfont.77c882e5.ttf"
+  },
+  {
+    "revision": "b80c083333768222d5cd98f155cf4006",
+    "url": "/static/media/iconfont.b80c0833.woff"
+  }
+]);
