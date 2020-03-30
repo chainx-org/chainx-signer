ccf6c6d849abcb2cbb1c4e5e38940daf4e257c5b
Yongfeng LI <wliyongfeng@gmail.com>
2020-03-30 22:02:19 +0800

    Improve node delay text

--- src/pages/Header/Delay.js
+++ src/pages/Header/Delay.js
@@ -2,6 +2,14 @@ import { getDelayClass } from './utils'
 import React from 'react'
 
 export default function({ delay }) {
+  function getDelayText(delay) {
+    if (delay > 1000) {
+      return `${(delay / 1000).toFixed(1)} s`
+    }
+
+    return `${delay} ms`
+  }
+
   return (
     <span className={getDelayClass(delay)}>
       {do {
@@ -13,7 +21,7 @@ export default function({ delay }) {
           ;('timeout')
         } else {
           // eslint-disable-next-line no-unused-expressions
-          ;`${delay} ms`
+          getDelayText(delay)
         }
       }}
     </span>
