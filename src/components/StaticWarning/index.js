efa9f96a29fa7924cf55db4b07613ade1b7ac2c5
Yongfeng LI <wliyongfeng@gmail.com>
2020-04-10 11:57:57 +0800

    Refactor StaticWarning

--- src/components/StaticWarning/index.js
+++ src/components/StaticWarning/index.js
@@ -1,7 +1,31 @@
 import React from 'react'
 // @ts-ignore
 import warningIcon from '../../assets/warning.png'
-import './index.scss'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  & > span {
+    margin-top: 16px;
+    font-size: 16px;
+    color: #3f3f3f;
+    font-weight: 500;
+  }
+  & > img {
+    width: 64px;
+    height: 56px;
+  }
+  & > div {
+    margin-top: 16px;
+    font-size: 14px;
+    width: 246px;
+    color: #666666;
+    text-align: center;
+  }
+`
 
 function StaticWarning(props) {
   const {
@@ -10,11 +34,11 @@ function StaticWarning(props) {
   } = props
 
   return (
-    <div className="static-warning">
-      <img className="warning-icon" src={warningIcon} alt="warning" />
-      <span className="warning-title">{title}</span>
-      <div className="warning-desc">{desc}</div>
-    </div>
+    <Wrapper>
+      <img src={warningIcon} alt="warning" />
+      <span>{title}</span>
+      <div>{desc}</div>
+    </Wrapper>
   )
 }
 
